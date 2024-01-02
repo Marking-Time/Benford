fc0b7e4e80076b9ae547c4f966039c32c4fd751a
Marking-Time <153015560+Marking-Time@users.noreply.github.com>
2024-01-02 22:15:52 +0000

    Add: Create Code

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #Benfords law
+import random
+one  = 0
+two = 0
+three = 0 
+#print(random.randint(0,9999))
+for i in range(0,1000):
+  i = random.randint(0,9999)
+  #print(i)
+  number = str(i)
+  #print(number)
+  if number[0] == "1":
+    one += 1
+
+  if number[0] == "2":
+    two += 1
+
+  if number[0] == "3":
+    three += 1
+
+print("ones = " + str(one))
+print("twos = " + str(two))
+print("threes = " + str(three))
+print("Calculated: "+str(one+two+three))
+print("Expected: " + str(10*(30.1+17.6+12.5)))
+    
